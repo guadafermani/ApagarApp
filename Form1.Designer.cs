ec8e24b191c51086aa9179b49b47812a49c5eefc
guadafermani <guadalupefermani@gmail.com>
2021-07-28 02:04:32 -0300

    primer versión

---
+++ Form1.Designer.cs
@@ -0,0 +1,112 @@
+﻿
+using System.Windows.Forms;
+
+namespace ApagarApp
+{
+    partial class ApagarApp
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dateTimePicker = new System.Windows.Forms.DateTimePicker();
+            this.acceptButton = new System.Windows.Forms.Button();
+            this.eraseButton = new System.Windows.Forms.Button();
+            this.statusLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // dateTimePicker
+            // 
+            this.dateTimePicker.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+            | System.Windows.Forms.AnchorStyles.Left)
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dateTimePicker.CalendarFont = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.dateTimePicker.CustomFormat = "dd/MM/yyyy HH:mm";
+            this.dateTimePicker.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.dateTimePicker.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.dateTimePicker.Location = new System.Drawing.Point(35, 16);
+            this.dateTimePicker.Name = "dateTimePicker";
+            this.dateTimePicker.Size = new System.Drawing.Size(426, 33);
+            this.dateTimePicker.TabIndex = 0;
+            this.dateTimePicker.ValueChanged += new System.EventHandler(this.dateTimePicker_ValueChanged);
+            // 
+            // acceptButton
+            // 
+            this.acceptButton.Location = new System.Drawing.Point(35, 127);
+            this.acceptButton.Name = "acceptButton";
+            this.acceptButton.Size = new System.Drawing.Size(75, 23);
+            this.acceptButton.TabIndex = 1;
+            this.acceptButton.Text = "Aceptar";
+            this.acceptButton.UseVisualStyleBackColor = true;
+            this.acceptButton.Click += new System.EventHandler(this.acceptButton_Click);
+            // 
+            // eraseButton
+            // 
+            this.eraseButton.Location = new System.Drawing.Point(386, 127);
+            this.eraseButton.Name = "eraseButton";
+            this.eraseButton.Size = new System.Drawing.Size(75, 23);
+            this.eraseButton.TabIndex = 1;
+            this.eraseButton.Text = "Borrar";
+            this.eraseButton.UseVisualStyleBackColor = true;
+            this.eraseButton.Click += new System.EventHandler(this.eraseButton_Click);
+            // 
+            // statusLabel
+            // 
+            this.statusLabel.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.statusLabel.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.statusLabel.Location = new System.Drawing.Point(35, 68);
+            this.statusLabel.Name = "statusLabel";
+            this.statusLabel.Size = new System.Drawing.Size(426, 31);
+            this.statusLabel.TabIndex = 2;
+            this.statusLabel.Text = "No se programó ningún apagado";
+            this.statusLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // ApagarApp
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(500, 169);
+            this.Controls.Add(this.statusLabel);
+            this.Controls.Add(this.eraseButton);
+            this.Controls.Add(this.acceptButton);
+            this.Controls.Add(this.dateTimePicker);
+            this.KeyPreview = true;
+            this.Name = "ApagarApp";
+            this.Text = "Apagar PC";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.ApagarApp_KeyPress);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private DateTimePicker dateTimePicker;
+        private Button acceptButton;
+        private Button eraseButton;
+        private Label statusLabel;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace ApagarApp
+{
+    public partial class ApagarApp : Form
+    {
+        public ApagarApp()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void acceptButton_Click(object sender, EventArgs e)
+        {
+            int secondsDifference = getSecondsDifference();
+
+            if (secondsDifference < 0)
+                return;
+
+            string arguments = "/s /t " + secondsDifference;
+            Process shutdownProcess = Process.Start("shutdown", arguments);
+            shutdownProcess.WaitForExit();
+            if (shutdownProcess.ExitCode == 1190)
+            {
+                Process.Start("shutdown", "/a").WaitForExit();
+                acceptButton_Click(acceptButton, new EventArgs());
+            }
+
+            statusLabel.Text = "Apagado programado para " + dateTimePicker.Text;
+            statusLabel.ForeColor = Color.Red;
+        }
+
+        private int getSecondsDifference()
+        {
+            string timeString = dateTimePicker.Text;
+            System.DateTime shutdownTime = System.DateTime.Parse(timeString);
+            System.DateTime actualTime = System.DateTime.Now;
+            TimeSpan timeSpan = shutdownTime - actualTime;
+
+            int secondsDifference = (int)timeSpan.TotalSeconds;
+
+            return secondsDifference;
+        }
+
+        private void eraseButton_Click(object sender, EventArgs e)
+        {
+            Process shutdownProcess = Process.Start("shutdown", "/a");
+            shutdownProcess.WaitForExit();
+
+            statusLabel.Text = "No se programó ningún apagado";
+            statusLabel.ForeColor = Color.Black;
+        }
+
+        private void dateTimePicker_ValueChanged(object sender, EventArgs e)
+        {
+            string timeString = dateTimePicker.Text;
+            System.DateTime shutdownTime = System.DateTime.Parse(timeString);
+            System.DateTime actualTime = System.DateTime.Now;
+            TimeSpan timeSpan = shutdownTime - actualTime;
+            int secondsDifference = (int)timeSpan.TotalSeconds;
+
+            if (secondsDifference < 0)
+            {
+                dateTimePicker.Text = System.DateTime.Now.ToString();
+            }
+        }
+
+        private void ApagarApp_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == (char)13)
+            {
+                acceptButton_Click(acceptButton, new EventArgs());
+            }
+        }
+    }
+}
